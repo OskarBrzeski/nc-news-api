c26e4c515347bea21f5efe78d9391e66e79a003d
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-02-20 15:24:42 +0000

    move work from controller to model

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -2,22 +2,9 @@ const {
     selectArticleById,
     selectAllArticles,
 } = require("../models/articles.models");
-const {
-    selectCommentCountsByArticleId,
-} = require("../models/comments.models");
 
 exports.getArticles = (req, res, next) => {
-    const promises = [selectAllArticles(), selectCommentCountsByArticleId()];
-
-    Promise.all(promises)
-        .then(([articles, commentsPerArticle]) => {
-            articles.forEach((article) => {
-                article.comment_count = commentsPerArticle[article.article_id] || 0;
-                delete article.body;
-            })
-
-            return articles
-        })
+    selectAllArticles()
         .then((articles) => {
             res.status(200).send({ articles });
         })
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,15 +1,23 @@
 const db = require("../db/connection");
 
 exports.selectAllArticles = () => {
-    return db
-        .query("SELECT * FROM articles ORDER BY created_at DESC")
-        .then(({ rows }) => {
-            rows.forEach((article) => {
-                article.created_at = fixTimestamp(article.created_at);
-            });
-            
-            return rows;
+    const query = `
+        SELECT a.author, a.title, a.article_id, a.topic,
+        a.created_at, a.votes, a.article_img_url,
+        CAST(COUNT(c.article_id) AS INTEGER) AS comment_count
+        FROM articles AS a
+        LEFT JOIN comments AS c ON a.article_id = c.article_id
+        GROUP BY a.article_id
+        ORDER BY a.created_at DESC;
+    `;
+
+    return db.query(query).then(({ rows }) => {
+        rows.forEach((article) => {
+            article.created_at = fixTimestamp(article.created_at);
         });
+
+        return rows;
+    });
 };
 
 exports.selectArticleById = (article_id) => {
