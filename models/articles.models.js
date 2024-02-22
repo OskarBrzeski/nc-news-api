af6ca1e07fe012a7ee1040f2f6b39af8bff61831
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-02-22 11:13:19 +0000

    add comment_count to output of GET /api/articles/:article_id endpoint

--- models/articles.models.js
+++ models/articles.models.js
@@ -43,21 +43,29 @@ exports.selectArticlesWithQuery = (topic) => {
 };
 
 exports.selectArticleById = (article_id) => {
-    return db
-        .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
-        .then(({ rows }) => {
-            if (rows.length === 0) {
-                return Promise.reject({
-                    status: 404,
-                    msg: "Not found",
-                    desc: "No article found with given ID",
-                });
-            }
+    const query = `
+        SELECT a.author, a.title, a.article_id, a.body, a.topic,
+        a.created_at, a.votes, a.article_img_url,
+        CAST(COUNT(c.article_id) AS INTEGER) AS comment_count
+        FROM articles AS a
+        LEFT JOIN comments AS c ON a.article_id = c.article_id
+        WHERE a.article_id = $1
+        GROUP BY a.article_id
+    `;
 
-            const article = rows[0];
-            article.created_at = fixTimestamp(article.created_at);
-            return article;
-        });
+    return db.query(query, [article_id]).then(({ rows }) => {
+        if (rows.length === 0) {
+            return Promise.reject({
+                status: 404,
+                msg: "Not found",
+                desc: "No article found with given ID",
+            });
+        }
+
+        const article = rows[0];
+        article.created_at = fixTimestamp(article.created_at);
+        return article;
+    });
 };
 
 exports.updateArticleVotes = (articleId, votes) => {
